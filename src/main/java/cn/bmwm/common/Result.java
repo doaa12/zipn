3e7a7a62130980949b1e08301491b511f180d34c
zhoupuyue <zhoupuyue@163.com>
2014-12-07 20:20:44 +0800

    属性无法输出问题

--- src/main/java/cn/bmwm/common/Result.java
+++ src/main/java/cn/bmwm/common/Result.java
@@ -12,70 +12,46 @@ public class Result extends HashMap<String,Object> implements Serializable {
 	
 	private static final long serialVersionUID = -1385662450638309282L;
 
-	private int version = 1;
-
-	private int flag;
-	
-	private String message;
-	
-	private Object data;
-	
 	public Result() {
 		
 	}
 	
 	public Result(int flag) {
-		this.flag = flag;
+		this.put("flag", flag);
 	}
 	
 	public Result(int flag, int version) {
-		this.flag = flag;
-		this.version = version;
+		this.put("flag", flag);
+		this.put("version", version);
 	}
 	
 	public Result(int flag, int version, String message) {
-		this.flag = flag;
-		this.version = version;
-		this.message = message;
+		this.put("flag", flag);
+		this.put("version", version);
+		this.put("message", message);
 	}
 	
 	public Result(int flag, int version, String message, Object data) {
-		this.flag = flag;
-		this.version = version;
-		this.message = message;
-		this.data = data;
+		this.put("flag", flag);
+		this.put("version", version);
+		this.put("message", message);
+		this.put("data", data);
 	}
 	
-	public int getFlag() {
-		return flag;
-	}
-
 	public void setFlag(int flag) {
-		this.flag = flag;
-	}
-
-	public String getMessage() {
-		return message;
+		this.put("flag", flag);
 	}
 
 	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public Object getData() {
-		return data;
+		this.put("message", message);
 	}
 
 	public void setData(Object data) {
-		this.data = data;
-	}
-
-	public int getVersion() {
-		return version;
+		this.put("data", data);
 	}
 
 	public void setVersion(int version) {
-		this.version = version;
+		this.put("version", version);
 	}
 
 }
